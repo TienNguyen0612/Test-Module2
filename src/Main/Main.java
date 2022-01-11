e05bc2614013737d7e30868b04b051b6ab7fe5be
TienNguyen0612 <sales08.taybactravel@gmail.com>
2022-01-11 10:22:43 +0700

    Can I pass MD2 ?

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import _System.RunSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        RunSystem runSystem = new RunSystem();
+        runSystem.menuOfSystem();
+    }
+}
